057b2ac9495cc2e946bfa65f08f948cc06a20595
Mason Johnson <mason@invariant.dev>
2024-11-04 14:08:58 -0700

    commented out the blog nav until/unless we decide to use it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
                         position: 'left',
                         label: 'Labs',
                     },
-                    { to: '/blog', label: 'Blog', position: 'left' },
+                    // { to: '/blog', label: 'Blog', position: 'left' },
                     { 
                         type: 'doc',
                         docId: 'introInvolved',
