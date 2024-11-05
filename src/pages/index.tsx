e29c76ed02314ad0a3be53f3e69e8170de23461e
Mason Johnson <mason@invariant.dev>
2024-11-04 17:56:28 -0700

    homepage updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -10,17 +9,10 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className={clsx("container", styles.logoCentered)}>
+        <img src="img/logo.svg" alt="Kids on Computers" title="Kids on Computers" />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="get-involved/donate">
-            Donate via PayPal 💰
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -31,7 +23,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Bringing technology to underprivileged kids worldwide">
+      description="Bringing technology to underprivileged kids worldwide.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
